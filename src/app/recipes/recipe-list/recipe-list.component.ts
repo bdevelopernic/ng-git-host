fcad391c1e5af68b88b36f8235a978236ebdbf91
DESKTOP-5M2C6AD\Brijesh <bgupta985@gmail.com>
2018-04-15 18:32:22 +0530

    all commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes:Recipe[]=[new Recipe('A Test Recipe','This is simply a test','https://upload.wikimedia.org/wikipedia/commons/b/bb/Dakbal-bokkeum.jpg'),
+new Recipe('A Test Recipe','This is simply a test','https://upload.wikimedia.org/wikipedia/commons/b/bb/Dakbal-bokkeum.jpg')];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
